49c73ccbf1f9be6951cc0003072b7fbd5463c053
anwarmoazam <anwar.palimarwar@gmail.com>
2022-10-06 21:47:55 +0530

    modification with setTimeout

--- project_1.js
+++ project_1.js
@@ -1,4 +1,4 @@
-let arr = [41,42,43,44,45,46,47,48,49,5031,32,33,34,35,36,37,38,39,40];
+let arr = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
 
 let div = document.createElement('div');
 
@@ -20,12 +20,12 @@ div.appendChild(result)
 document.body.appendChild(div);
 
 function printTable(event){
-    let txt = "";
     let value = event.target.innerText;
-    txt = `Table of ${value} is : <br>`;
+    result.innerHTML = `Table of ${value} is : <br>`;
     for(let i=1; i<=10; i++){
-        txt += `${value} x ${i} : ${value*i}<br>`;
+        setTimeout(function(){
+            result.innerHTML += `${value} x ${i} = ${value*i}<br>`
+        },i*200)
     }
-    result.innerHTML = txt;
 }
 
