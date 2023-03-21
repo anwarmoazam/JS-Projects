c48564cc981d23a163ca0beef86397e5a44def8e
anwarmoazam <anwar.palimarwar@gmail.com>
2023-03-22 02:14:44 +0530

    project to-do

--- to-do.js
+++ to-do.js
@@ -4,13 +4,63 @@ let taskDetail = document.getElementById('task-detail');
 let taskDate = document.getElementById('task-date');
 let addToDoBtn = document.getElementById('addToDo');
 
-const category = ["Personal","Work","Lecture","Other"];
 
-const toDoList = [];
-const dueToDoList = [];
-const doneToDoList = [];
+
+
+const toDoList = {
+    // category: ["Personal","Work","Lecture","Other"];
+    personal : [
+        {
+            id : 1,
+            detail : 'Shopping',
+            date : new Date(),
+            done : false
+        },
+        {
+            id : 2,
+            detail : 'Watch Movie',
+            date : new Date(),
+            done : false
+        }
+    ],
+    work : [
+        {
+            id : 1,
+            detail : 'Make Report',
+            date : new Date(),
+            done : false
+        },
+        {
+            id : 2,
+            detail : 'Attend VC',
+            date : new Date(),
+            done : false
+        }
+    ],
+    study : [
+        {
+            id : 1,
+            detail : 'Comlpete Course',
+            date : new Date(),
+            done : false
+        },
+        {
+            id : 2,
+            detail : 'Code',
+            date : new Date(),
+            done : false
+        }
+    ]
+};
+
+
+
+// const toDoList = [];
+// const dueToDoList = [];
+// const doneToDoList = [];
 
 (function renderCategory(){
+    const category = Object.keys(toDoList);
     for(let i=0; i<category.length; i++){
         let option = document.createElement('option');
         option.innerText = category[i];
@@ -21,15 +71,19 @@ const doneToDoList = [];
 addToDoBtn.addEventListener('click',function(e){
     e.preventDefault();
     let toDo = {};
-    if((categoryDOM.value === "") || (taskDetail.value === "")){
+    if((categoryDOM.value.trim() === "") || (taskDetail.value.trim() === "")){
         alert("Please Insert Valid Detail");
         resetToDoForm();
     } else{
-        toDo.category = categoryDOM.value;
-        toDo.detail = taskDetail.value;
-        toDo.toDoDate = new Date(taskDate.value);
-        toDo.done = false;
-        toDoList.push(toDo);
+        for(let toDoCategory in toDoList){
+            if(categoryDOM.value === toDoCategory){
+                toDo.id = toDoList[toDoCategory].length+1;
+                toDo.detail = taskDetail.value;
+                toDo.toDoDate = new Date(taskDate.value);
+                toDo.done = false;
+                toDoList[toDoCategory].push(toDo);
+            }
+        }
         resetToDoForm();
         console.log(toDoList);
     }
