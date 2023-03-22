aa66a2f20e2a73998315ef2bde8dec7d86b042cb
anwarmoazam <anwar.palimarwar@gmail.com>
2023-03-22 20:23:17 +0530

    project to-do

--- to-do.js
+++ to-do.js
@@ -1,3 +1,33 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click',()=>{
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click',()=>{
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+function openModal(modal){
+    if(modal === null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal){
+    if(modal === null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
 
 let categoryDOM = document.getElementById('task-category');
 let taskDetail = document.getElementById('task-detail');
@@ -5,7 +35,7 @@ let taskDate = document.getElementById('task-date');
 let addToDo = document.getElementById("modal-open");
 let saveToDoBtn = document.getElementById('saveToDo');
 
-const toDoList = {
+let toDoList = {
     personal : [
     ],
     work : [
@@ -34,25 +64,24 @@ saveToDoBtn.addEventListener('click',function(e){
         alert("Please Insert Valid Detail");
         resetToDoForm();
     } else{
+        toDoList = JSON.parse(localStorage.getItem('toDoList'));
         for(let toDoCategory in toDoList){
             if(categoryDOM.value === toDoCategory){
                 toDo.id = toDoList[toDoCategory].length+1;
                 toDo.detail = taskDetail.value;
-                toDo.date = JSON.stringify(new Date(taskDate.value));
+                toDo.date = new Date(taskDate.value);
+                // toDo.date = JSON.stringify(new Date(taskDate.value).trim());
                 toDo.done = false;
+                console.log(toDo);
+                toDoList = JSON.parse(localStorage.getItem('toDoList'));
                 toDoList[toDoCategory].push(toDo);
                 localStorage.setItem('toDoList',JSON.stringify(toDoList));
             }
         }
         resetToDoForm();
-        console.log(toDoList);
     }
 })
 
-addToDo.addEventListener('click',function(){
-    console.log('add button clicked');
-    addToDo.classList.add("add-modal");
-})
 
 function resetToDoForm(){
     categoryDOM.value = "";
@@ -60,3 +89,48 @@ function resetToDoForm(){
     taskDate.value = "";
 }
 
+// for due to-do
+// for (const key in JSON.parse(localStorage.getItem('toDoList'))) {
+//     const dueToDo = JSON.parse(localStorage.getItem('toDoList'))[key].filter(item => {
+//         return (item.done == false);
+//     })
+//     console.log(dueToDo);
+// }
+
+// for Upcoming to-do
+// for(const key in JSON.parse(localStorage.getItem('toDoList'))){
+//     JSON.parse(localStorage.getItem('toDoList'))[key].forEach(element => {
+//         let toDoDate = new Date(element.date);
+//         let today = new Date();
+//         (toDoDate > today) ? console.log(element) : "No Upcoming To-do";
+//     });;
+// }
+
+// for complete to-do
+// for (const key in JSON.parse(localStorage.getItem('toDoList'))) {
+//     const completeToDo = JSON.parse(localStorage.getItem('toDoList'))[key].filter(element => {
+//         let toDoDate = new Date(element.date);
+//         let today = new Date();
+//         return (toDoDate < today) && (element.done==true)
+//     })
+//     console.log('Completed To-Do : ', key, completeToDo);
+// }
+
+// for Incomplete to-do
+// for (const key in JSON.parse(localStorage.getItem('toDoList'))) {
+//     const incompleteToDo = JSON.parse(localStorage.getItem('toDoList'))[key].filter(element => {
+//         let toDoDate = new Date(element.date);
+//         let today = new Date();
+//         return (toDoDate < today) && (element.done==false)
+//     })
+//     console.log('Incomplete To-Do : ', key, incompleteToDo);
+// }
+
+// for Inbox to-do
+function filterToDo(){
+    for (const key in JSON.parse(localStorage.getItem('toDoList'))) {
+        console.log(JSON.parse(localStorage.getItem('toDoList'))[key]);
+    }
+}
+
+filterToDo();
